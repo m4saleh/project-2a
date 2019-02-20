120ba1cd34cb11350576fbb2c5f5fc4e0dd593e2
Mohammed <4m.4s.2013@gmail.com>
2019-02-20 17:05:43 +0300

    Updates

---
+++ precache-manifest.5156400f0d20a65041cc105ad8f0cb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "171c315ad5e931264147",
+    "url": "/m4saleh.github.io/project-2a/static/css/main.ba439137.chunk.css"
+  },
+  {
+    "revision": "171c315ad5e931264147",
+    "url": "/m4saleh.github.io/project-2a/static/js/main.171c315a.chunk.js"
+  },
+  {
+    "revision": "dbeecbcb4a8c65b5cad5",
+    "url": "/m4saleh.github.io/project-2a/static/js/runtime~main.dbeecbcb.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/m4saleh.github.io/project-2a/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "8275fb1b032024ea43990510d091a5b8",
+    "url": "/m4saleh.github.io/project-2a/index.html"
+  }
+];
