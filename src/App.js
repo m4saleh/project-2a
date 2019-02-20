dab9ba1148cb4d895fd1aa594ec74974c73c2b55
Mohammed <4m.4s.2013@gmail.com>
2019-02-20 15:38:18 +0300

    mr projct 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Child from './child';
 
 class App extends Component {
+  //input 
+  state = {
+    formData: {
+      textList: ''
+    },
+
+
+    listArray:[],
+  }
+
+
+  updateForm = (event) => {
+    //
+    const newData = event.target.value;
+    //
+    const originalState = this.state.formData
+    //
+    const copy = Object.assign({}, originalState);
+    //
+    const key = event.target.name
+    //
+    copy[key] = newData
+    //
+    this.setState({
+      formData: copy,
+    })
+  }
+  submitForm = (event) => {
+    //function use for no refresh page
+    event.preventDefault()
+    //copy the arry start with index 0 
+    const copy = this.state.listArray.slice(0)
+    //take the arry and push the nwe 
+    copy.push(this.state.formData)
+    //copy the array and update change 
+    this.setState({
+      listArray: copy,
+      formData: {
+        textList:'',
+      }
+    })
+
+  }
+  print =()=>{
+   console.log('√√');
+  }
+  clear=()=>{
+    this.setState({ listArray: [] });
+  }
+
+  deleteList = (index) => {
+    console.log('deleting item # ', index)
+
+    var array = this.state.listArray.slice(0);
+    console.log('arr val: ', array)
+    array.splice(index, 1);
+    console.log('arr val 2: ', array)
+    this.setState({ listArray: array}) 
+
+    // this.setState({this.state.formData.listArray.splice(index, 1)}  ) 
+    
+  }
+  
   render() {
+    //take array for map and call up the child , style and list array 
+    const listInput = this.state.listArray.map((task , index) => {
+      return <Child text={task.textList} index={index} deleteList={this.deleteList}/> 
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+      
+        <form onSubmit={this.submitForm} >
+        
+          <label>
+            <h1 className='addYour'>add your ToDo List:
+        <input type="text" name="textList" className="inputText" onChange={this.updateForm} value={this.state.formData.textList} />
+              <input type="submit" value="Submit" className="subm"/>
+            </h1>
+            
+          </label>
+         
+         
+        </form>
+        {listInput}
+        <button type="reset" value="Reset" className="clearButton" onClick={this.clear}>Clear</button>
+
+      
+        
+
       </div>
     );
   }
---
+++ src/child.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+
+class Child extends Component {
+state = {
+    cross:false,
+    // uncross:'false'
+    crossClass: ""
+    }
+
+
+// checkLine = () =>{
+//     if (this.state.cross === 'false'){
+//         this.setState({cross: 'true'} )
+//         console.log('cross');
+        
+//     } else if (this.state.uncross === 'false'){
+//         this.setState({ uncross: 'true' })
+//         console.log('uncross');
+//     }
+// }
+
+
+    checkLine = () => {
+        //call up the cross
+        if (this.state.crossClass === ""){
+            //give css for cross when cross false
+            this.setState({ 
+                cross: false, 
+                crossClass: "cross"
+            })
+        }else {
+            //when cross true take this css
+            this.setState({ 
+                cross: true, 
+                crossClass: ""
+            })
+        }
+
+        
+    }
+    // clearOneList = (number)=>{
+    //     // this.setState(this.listArray.remove(number));
+    //     // this.listArray
+    //     this.setState();
+
+    //     // array.remove(number);
+    //     console.log('Clear on list');
+        
+    // }
+    render() {
+
+        return (
+            <div className={this.state.crossClass}>
+                <input  type="checkbox" className="check"  onChange={ this.checkLine} />
+                {this.props.text}
+                <bytton type="reset" value="Reset" className="redX" onClick={() => this.props.deleteList(this.props.index)}>✕</bytton >
+                
+            </div >
+        );
+    }
+}
+
+export default Child;
